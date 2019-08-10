676c3e3f401f4835abb6df0d06f787d3a4f24e0f
Ramya_B11 <Ramya_B11@CHNSHL147826L.ad.infosys.com>
2019-08-10 17:45:17 +0530

    pushing these codes

---
+++ 1/src/main/java/com/leafBot/selenium/pages/SeleniumPages.java
@@ -0,0 +1,32 @@
+package com.leafBot.selenium.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler;
+
+import com.leaftaps.base.ProjectSpecificMethods;
+
+public class SeleniumPages extends ProjectSpecificMethods {
+
+	public   SeleniumPages() {
+	
+	}
+	
+	public void enterUserName(WebElement ele, String data) {
+		locateElement("id", "username");
+	
+	
+		
+	
+		
+	
+
+	
+	}	
+
+
+//	public FindLeads clickFindLead() {
+//		driver.findElementByLinkText("Find Leads").click();
+//
+//		return new FindLeads();
+
+	}
